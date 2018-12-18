b4f46b9d7ee6ea283e01f0e4bc5d1ab532e71471
Benjamin Lupton <b@lupton.cc>
2018-12-18 11:06:15 +1100

    add js file to showcase

---
+++ source/index.js
@@ -0,0 +1,3 @@
+const badges = require('badges')
+
+// badges.
--- source/index.ts
+++ source/index.ts
@@ -4,3 +4,5 @@ import * as badges from 'badges'
 Could not find a declaration file for module 'badges'. '/Users/balupton/Projects/active/typescript-consume-jsdoc/node_modules/badges/index.js' implicitly has an 'any' type.
 	Try`npm install @types/badges` if it exists or add a new declaration(.d.ts) file containing`declare module 'badges';`[7016]
 */
+
+// badges.
