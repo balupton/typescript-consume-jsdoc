29cdb0a24c278b93138d9cb1c636a6f966c54c5b
Benjamin Lupton <b@lupton.cc>
2018-12-18 11:05:19 +1100

    initial typescript

---
+++ source/index.ts
@@ -0,0 +1,6 @@
+import * as badges from 'badges'
+/*
+[ts]
+Could not find a declaration file for module 'badges'. '/Users/balupton/Projects/active/typescript-consume-jsdoc/node_modules/badges/index.js' implicitly has an 'any' type.
+	Try`npm install @types/badges` if it exists or add a new declaration(.d.ts) file containing`declare module 'badges';`[7016]
+*/
